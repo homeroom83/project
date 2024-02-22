c82d12b9a71d7df6194725e5d04288f961480daf
homeroom83 <st655277@gmail.com>
2024-02-22 23:33:43 +0800

    admin-product-v2

--- front/src/views/admin/ProductsView.vue
+++ front/src/views/admin/ProductsView.vue
@@ -1,9 +1,12 @@
 <template>
   <v-navigation-drawer permanent>
     <v-container>
-      <v-btn >新增商品</v-btn>
-    </v-container>
-    <v-container>
+      <v-row>
+        <v-col class="text-center">商品管理</v-col>
+      </v-row>
+      <v-col>
+        <v-btn>新增商品</v-btn>
+      </v-col>
       <v-list>
         <v-list-item v-for="item in list" :key="item">{{ item.text }}</v-list-item>
       </v-list>
--- front/src/views/front/HomeView.vue
+++ front/src/views/front/HomeView.vue
@@ -1,3 +1,3 @@
-<template lang="pug">
-h1 123
+<template>
+  <h1>home</h1>
 </template>
